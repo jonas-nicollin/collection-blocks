3fd204f1936e0966e00fa3d5df5801d8c3ea024b
jonas-nicollin <45790197+jonas-nicollin@users.noreply.github.com>
2026-07-28 13:52:21 +0200

    Update and rename dev_collection-blocks-v019.js to dev_collection-blocks-v020.js

--- dev_collection-blocks-v020.js
+++ dev_collection-blocks-v020.js
@@ -865,6 +865,12 @@
             return d1.day + '\u2013' + d2.day + '\u00a0' + monthLabel + '\u00a0' + d1.year;
           }
 
+          if (d1.year !== d2.year) {
+            return dt1.toLocaleDateString(loc, { day: 'numeric', month: 'long', year: 'numeric' }) +
+              '\u2013' +
+              dt2.toLocaleDateString(loc, { day: 'numeric', month: 'long', year: 'numeric' });
+          }
+
           return dt1.toLocaleDateString(loc, { day: 'numeric', month: 'long' }) +
             '\u2013' +
             dt2.toLocaleDateString(loc, { day: 'numeric', month: 'long', year: 'numeric' });
